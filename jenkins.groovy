14b79491135648ca59356ca715b2ce481fd4e661
Daniil Zemnov <zemnowf@gmail.com>
2022-11-03 21:38:28 +0300

    fix jenkins config

--- jenkins.groovy
+++ jenkins.groovy
@@ -82,7 +82,7 @@ def generateAllure() {
             jdk              : '',
             properties       : [],
             reportBuildPolicy: 'ALWAYS',
-            results          : [[path: 'final-project/build/allure-results']]
+            results          : [[path: 'build/allure-results']]
     ])
 }
 
